c34d75d4d24002f00852f7f51592926a1c0e8505
cristian-sima <cs25g13@soton.ac.uk>
2016-11-11 21:02:02 +0200

    fix operator

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 require_once 'include/php/Aplicatie.php';
 
+Login::permiteOperator();
+
 if(Aplicatie::getInstance()->getUtilizator()->isAdministrator()) {
   header("Location: pagina_principala.php");
 } else {
