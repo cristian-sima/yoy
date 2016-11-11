3cfb5e198f6879099a1025f12e02a89be89600a1
cristian-sima <cs25g13@soton.ac.uk>
2016-11-11 20:57:18 +0200

    fix

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
 require_once 'include/php/Aplicatie.php';
 
-header("Location: pagina_principala.php");
+if(Aplicatie::getInstance()->getUtilizator()->isAdministrator()) {
+  header("Location: pagina_principala.php");
+} else {
+  header("Location: situatie_mecanica_operator.php");
+}
