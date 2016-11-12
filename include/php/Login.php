345daefca025b991f7ad284bfd00506eea0e9d91
cristian-sima <cs25g13@soton.ac.uk>
2016-11-12 15:24:01 +0200

    finish login + mysql

--- include/php/Login.php
+++ include/php/Login.php
@@ -1,344 +1,307 @@
 <?php
-
 require_once "Utilizator.php";
 
-/**
- *
- * Contine informatii despre login-ul unui utilizator la aplicatie
- * @author			Cristian Sima
- * @data			12.02.2014
- * @version			1.2
- *
- */
-class Login
-{
-	private static $mysql				= null;
-	private static $id_user				= null;
-	private static $allowOperator		= false;
-
-	/**
-	 *
-	 * @description 			Verifica autentificarea prin sesiune - daca utilizatorul este deja autentificat  verifica daca utilizatorul a fost memorat in cookie adaug o sesiune ca utilizatoru este logat Apeleaza functia ce verifica daca datele din sesiune sunt corecte cu cele din baza de date returneaza TRUE daca utilizatorul este autentificat.
-	 * @param MYSQL $mysql 		Referinta spre obiectul cu conexiunea sql
-	 * @throws Exception 		In cazul in care exista o problema la logare
-	 *
-	 */
-	public static function request_access(MYSQL $mysql)
-	{
-
+class Login {
+	private static $mysql = null;
+	private static $id_user = null;
+	private static $allowOperator = false;
+	public static function request_access(MYSQL $mysql) {
 		self::$mysql = $mysql;
-
-
-		/* verifica daca utilizatorul este memorat in cookie */
-		if(isset($_COOKIE['cookuser']) && isset($_COOKIE['cookpass']))
-		{
-			$_SESSION['user'] 		= $_COOKIE['cookuser'];
-			$_SESSION['parola'] 	= $_COOKIE['cookpass'];
+		if (isset($_COOKIE['cookuser']) && isset($_COOKIE['cookpass'])) {
+			$_SESSION['user']   = $_COOKIE['cookuser'];
+			$_SESSION['parola'] = $_COOKIE['cookpass'];
 		}
+		if (isset($_SESSION['user']) && isset($_SESSION['parola'])) {
 
+			$db = $mysql->getResource();
 
-		/* verifica daca exista in sesiune numele și parola setate */
-		if(isset($_SESSION['user']) && isset($_SESSION['parola']))
-		{
-			/* Confirma daca numele și parola sunt valide */
+			$query = (
+				"SELECT _temp, tipCont
+				FROM `utilizator`
+				WHERE `user`= :user"
+			);
 
+			$stmt = $db->prepare($query);
+			$ok = $stmt->execute(array(
+				'user' => $_SESSION['user']
+			));
 
-			$sql1 = "SELECT _temp,tipCont from `utilizator` WHERE `user`='".$_SESSION['user']."'";
-			$result2 = mysql_query($sql1, $mysql->getResource());
-
-			/* Gaseste nr. incercari asociata user */
-			while($user3 = mysql_fetch_array($result2))
-			{
-				$temp  = $user3['_temp'];
-				$type  = $user3['tipCont'];
+			if(!$ok) {
+				throw new Exception("Ceva nu a mers cum trebuia");
 			}
 
+			foreach($stmt as $row) {
+				$temp = $row['_temp'];
+				$type = $row['tipCont'];
+			}
 
-
-			if($type=='normal' && $temp!=0&& ($temp < (time()-600)) )
-			{
+			if ($type == 'normal' && $temp != 0 && ($temp < (time() - 600))) {
 				unset($_SESSION['user']);
 				unset($_SESSION['parola']);
-				setcookie("cookuser", "", time()-60*60*24*100, "/");
-				setcookie("cookpass", "", time()-60*60*24*100, "/");
-
+				setcookie("cookuser", "", time() - 60 * 60 * 24 * 100, "/");
+				setcookie("cookpass", "", time() - 60 * 60 * 24 * 100, "/");
 				throw new Exception('Ai fost deconectat automat pentru că au trecut mai mult de 10 minute de la ultima acțiune. Te poți re-conecta <a href="index.php">aici</a></center>');
+			} else {
+
+				$query = (
+					"UPDATE utilizator
+					SET _temp=:temp
+					WHERE user=:user
+					LIMIT 1"
+				);
+
+				$stmt = $db->prepare($query);
+				$ok = $stmt->execute(array(
+					'temp' => "0",
+					'user' => $_SESSION['user']
+				));
+
+				if(!$ok) {
+					throw new Exception("Ceva nu a mers cum trebuia");
+				}
 			}
-			else
-			{
-				$q = "UPDATE utilizator SET _temp='0' WHERE user='".$_SESSION['user']."' LIMIT 1" ;
-				$result = mysql_query($q, $mysql->getResource());
-			}
-
-
-			$return =  self::confirmUser($_SESSION['user'], $_SESSION['parola']);
-
-
-			if($return != 0)
-			{
-				/* daca variabilele sunt incorecte, sterge sesiunea, utilizatorul nu este logat */
+			$return = self::confirmUser($_SESSION['user'], $_SESSION['parola']);
+			if ($return != 0) {
 				unset($_SESSION['user']);
 				unset($_SESSION['parola']);
-
 				throw new Exception("A intervenit o problemă cu conectarea. Vă sfătuim să mai încercați încă o dată conectarea.");
+			} else {
+
+				$query = (
+					"UPDATE utilizator
+					SET _temp=:temp
+					WHERE user=:user
+					LIMIT 1"
+				);
+
+				$stmt = $db->prepare($query);
+				$ok = $stmt->execute(array(
+					'temp' =>  time(),
+					'user' => $_SESSION['user']
+				));
+
+				if(!$ok) {
+					throw new Exception("Ceva nu a mers cum trebuia");
+				}
 			}
-			else
-			{
-				$q = "UPDATE utilizator
-					SET 	_temp='".time()."'
-					WHERE 	user='".$_SESSION['user']."'
-					LIMIT 1" ;
-				$result = mysql_query($q, self::$mysql->getResource());
-			}
-		}
-		else
-		{
-			if(isset($_POST['sublogin']) && isset($_POST['user']) && isset($_POST['pass']))
-			{
-				// Elimina spatiile din marginile numelui și parolei
+		} else {
+			if (isset($_POST['sublogin']) && isset($_POST['user']) && isset($_POST['pass'])) {
 				$_POST['user'] = trim($_POST['user']);
-				// $_POST['pass'] = trim($_POST['pass']);
-
-				/* verifica daca toate campurile au fost completate */
-				if(!$_POST['user'] || !$_POST['pass']){
-					$eroare='Nu ati completat toate campurile';
-				}
-
-				/* verifica lungimea numelui */
-				else if(strlen($_POST['user'])<3 || strlen($_POST['user'])>32) {
-					$eroare='Numele trebuie să conțină între 3 și 32 caractere';
-				}
-
-				else
-				{
-					// Verifica și adauga incercarea de logare in tabelul user_temp
+				if (!$_POST['user'] || !$_POST['pass']) {
+					$eroare = 'Nu ati completat toate campurile';
+				} else if (strlen($_POST['user']) < 3 || strlen($_POST['user']) > 32) {
+					$eroare = 'Numele trebuie să conțină între 3 și 32 caractere';
+				} else {
 					$continua = self::temp_user($_POST['user']);
-
-					if ($continua=='continua')
-					{
-
+					if ($continua == 'continua') {
 						$parola  = $_POST['pass'];
-						$md5pass =  md5($_POST['pass']);		// Cripteaza parola
+						$md5pass = md5($_POST['pass']);
+						$result  = self::confirmUser($_POST['user'], $md5pass);
+						if ($result == 1) {
+							$eroare = 'Numele <b>' . stripslashes($_POST['user']) . '</b> nu este înregistrat sau contul a fost dezactivat de către administrator';
+						} else if ($result == 2) {
+							$eroare = 'Parola este incorectă.';
+						} else if ($result == 3) {
+							$eroare = ('<center><br /><br /><font color="red"><h3>Înregistrarea pt. <u>' . stripslashes($_POST['user']) . '</u> este neconfirmată.</h3></font> Verificați contul de e-mail folosit la înregistrare (inclusiv în Spamm) pt. mesajul cu link-ul de confirmare.<br /><br /> </center>');
+						} else {
 
-						/* verifica daca numele este in baza de date și parola este corecta */
-						$result = self::confirmUser($_POST['user'], $md5pass);
+							$query = (
+								"UPDATE utilizator
+								SET _temp=:temp
+								WHERE user=:user
+								LIMIT 1"
+							);
 
-						/* Seteaza mesajul in cazul in care user sau parola sunt incorecte */
-						if($result==1) {
-							$eroare='Numele <b>'. stripslashes($_POST['user']). '</b> nu este înregistrat sau contul a fost dezactivat de către administrator';
-						}
-						else if($result==2)
-						{
-							$eroare='Parola este incorectă.';
-						}
-						else if($result==3)
-						{
-							$eroare = ('<center><br /><br /><font color="red"><h3>Înregistrarea pt. <u>'. stripslashes($_POST['user']). '</u> este neconfirmată.</h3></font> Verificați contul de e-mail folosit la înregistrare (inclusiv în Spamm) pt. mesajul cu link-ul de confirmare.<br /><br /> </center>');
-						}
-						else
-						{
+							$stmt = $db->prepare($query);
+							$ok = $stmt->execute(array(
+								'temp' => 0,
+								'user' => $_POST['user']
+							));
 
-							$q = "UPDATE utilizator SET _temp='0' WHERE user='".$_POST['user']."' LIMIT 1" ;
-							$result = mysql_query($q, $mysql->getResource());
+							if(!$ok) {
+								throw new Exception("Ceva nu a mers cum trebuia");
+							}
 
-							/* daca numele și parola sunt corecte, inregistreaza variabilele in sesiune */
-							$_SESSION['user'] = $_POST['user'];
+							$_SESSION['user']   = $_POST['user'];
 							$_SESSION['parola'] = $md5pass;
-
-							setcookie("cookuser", $_SESSION['user'], time()+60*60*24*100, "/");
-							setcookie("cookpass", $md5pass, time()+60*60*24*100, "/");
-
-
+							setcookie("cookuser", $_SESSION['user'], time() + 60 * 60 * 24 * 100, "/");
+							setcookie("cookpass", $md5pass, time() + 60 * 60 * 24 * 100, "/");
 							PAGE::showCSSLogin();
 							PAGE::showConfirmation('Am stabilit conexiunea cu serverul ! <br />Aplicația se încarcă...');
-
-							/* Auto redirect pentru a evita retrimiterea datelor cand da inapoi sau la refresh */
 							echo '<meta http-equiv="Refresh" content="0;url=index.php">';
 							die();
 						}
-					}
-					else
-					{
-						// Seteaza mesajul cu timpul ramas pana la o noua incercare de autentificare
-						$continua = floor($continua/60). ' minute, '. ($continua%60). ' secunde';
-						$eroare = 'Ați depășit numarul de încercari permise pt. autentificare. Puteți reîncerca după <br /><b>'. $continua. '</b>';
+					} else {
+						$continua = floor($continua / 60) . ' minute, ' . ($continua % 60) . ' secunde';
+						$eroare   = 'Ați depășit numarul de încercari permise pt. autentificare. Puteți reîncerca după <br /><b>' . $continua . '</b>';
 					}
 				}
-					throw new Exception($eroare);
-			}
-			else
-			{
+				throw new Exception($eroare);
+			} else {
 				throw new Exception("Această pagină necesită conectare !");
 			}
 		}
-
 	}
-
-	/**
-	 * Returneaza id-ul utilizatorului care este conectat la aplicatie
-	 * @return int				ID-ul utilizatorului care este conectat la aplicatie
-	 */
-	public static function getUserId()
-	{
+	public static function getUserId() {
 		return self::$id_user;
-
 	}
-
-	/**
-	 * Permite accesul la aplicatie petru operatori. In mod prestabilit posibilitate de accesare este doar pentru administratori
-	 */
-	public static function permiteOperator()
-	{
+	public static function permiteOperator() {
 		self::$allowOperator = true;
 	}
+	private static function confirmUser($clientUser, $clientPassword) {
 
-	/**
-	 *
-	 * Verifica daca user exista in baza de date daca da, verifica daca parola se potriveste cu cea din baza de date daca numele sau parola nu sunt corecte, returneaza eroarea (1 sau 2). pentru nume și parola confirmate returneaza 0.
-	 *
-	 * @param String $user			Utilizatorul care va fi verificat
-	 * @param String $parola		Parola utilizatorului criptata md5
-	 * @throws  Exception			Utilizator care nu se afla in baza de date sau nu este activ
-	 * @throws  Exception			Parola incorecta
-	 * @return						ID-ul utilizatorului
-	 *
-	 */
-	private static function confirmUser($user, $parola)
-	{
+		$db = self::$mysql->getResource();
 
-		/* Adauga slashuri daca este necesar (pentru query) */
-		if(!get_magic_quotes_gpc())
-		{
-			$user = addslashes($user);
-			$parola = addslashes($parola);
-		}
+		$query = (
+			"SELECT parola, id, tipCont
+			FROM utilizator
+			WHERE user=:user AND activ=:active
+			LIMIT 1"
+		);
 
+		$stmt = $db->prepare($query);
+		$ok = $stmt->execute(array(
+			'user' => $clientUser,
+			'active' => '1'
+		));
 
-		/* Verifica daca numle este in baza de date */
-		$mysql_ = "	SELECT parola,id
-				FROM `utilizator`
-				WHERE `user`='$user' AND activ='1' ".((self::$allowOperator)?(""):("AND tipCont = 'admin'"))."
-				LIMIT 1";
-
-		$result = mysql_query($mysql_, self::$mysql->getResource());
-
-
-		if(!$result || (mysql_num_rows($result)<1))
-		{
+		if(!$ok) {
 			return 1;
 		}
-		else
-		{
-			/* Gaseste parola asociata numelui */
-			$dbarray 					= mysql_fetch_array($result);
-			$parola_db  				= stripslashes($dbarray['parola']);
-			$id_db				  		= stripslashes($dbarray['id']);
 
-			self::$id_user 				= $id_db;
+		foreach($stmt as $row) {
+			$currentPassword = $row['parola'];
+			$currentID = $row["id"];
+			$currentType = $row["tipCont"];
 
-			/* Verifica daca parola scrisa este aceeasi cu cea gasita in baza de date */
-			if($parola == $parola_db)
-			{
+			// check the type of user
+			if (!self::$allowOperator && $currentType !== "admin") {
+				return 1;
+			}
+
+			if ($clientPassword == $currentPassword) {
 				return 0;
 			}
-			else
-			{
-				return 2;
-			}
+
+			// save id
+			self::$id_user = $currentID;
+
+			return 2;
 		}
+
+		return 1;
 	}
-
-	/**
-	 * Șterge cook-urile, datele de sesiune și implicit deconecteaza utilizatorul conectat
-	 */
-	public static function disconnect()
-	{
-		if(isset($_COOKIE['cookuser']) && isset($_COOKIE['cookpass']))
-		{
-			setcookie("cookuser", "", time()-60*60*24*100, "/");
-			setcookie("cookpass", "", time()-60*60*24*100, "/");
+	public static function disconnect() {
+		if (isset($_COOKIE['cookuser']) && isset($_COOKIE['cookpass'])) {
+			setcookie("cookuser", "", time() - 60 * 60 * 24 * 100, "/");
+			setcookie("cookpass", "", time() - 60 * 60 * 24 * 100, "/");
 		}
-
-		if(isset($_SESSION['user']))
-		{
-			/* Șterge sesiunea */
+		if (isset($_SESSION['user'])) {
 			unset($_SESSION['user']);
 			unset($_SESSION['parola']);
-			$_SESSION = array(); // reseteaza matricea sesiunii
-			@session_destroy();   // sterge toate sesiunile.
-
+			$_SESSION = array();
+			@session_destroy();
 			PAGE::showCSSLogin();
 			Page::showConfirmation("Ai fost deconectat cu succes ! <br /> Redirecționare la pagina principală în 3 secunde. <meta http-equiv='Refresh' content='3;url=index.php'>");
-		}
-		else
-		{
+		} else {
 			echo " <meta http-equiv='Refresh' content='0;url=index.php'>";
 		}
 	}
+	private static function temp_user($user) {
+		$data       = time();
+		$data_expir = $data - 601;
+		$ip         = $_SERVER['REMOTE_ADDR'];
 
-	/**
-	 *
-	 * 	Urmatoarea functie sterge randurile din tabelul user_temp mai vechi de 10 min. verifica daca utilizatorul a incercat de mai multe ori autentificarea daca a incercat deja de 3 ori fara nume-parola confirmate solicita incercarea unei noi autentificari dupa 10 min
-	 * @param string $user					Utilizatorul care va fi testat [@String]
-	 *
-	 */
-	private static function temp_user($user)
-	{
-		$data = time();
-		$data_expir = $data-601;
-		$ip = $_SERVER['REMOTE_ADDR'];
+		$db = self::$mysql->getResource();
 
-		/* Șterge randurile din tabelul user_temp mai vechi de 10 min */
-		$sql = "DELETE FROM `user_temp`
-					WHERE `data`<$data_expir";
-		mysql_query($sql, self::$mysql->getResource());
+		// delete old tokens
 
-		/* Adauga slashuri (pentru nume), daca PHP nu e setat sa adauge implicit */
-		if(!get_magic_quotes_gpc())
-		{
-			$user = addslashes($user);
+		$query = (
+			"DELETE from user_temp
+			WHERE data <:data"
+		);
+
+		$stmt = $db->prepare($query);
+		$ok = $stmt->execute(array(
+			'data' => $data_expir
+		));
+
+		if(!$ok) {
+			throw new Exception("Ceva nu a mers cum trebuia");
 		}
 
-		/* Verifica daca numle este in tabelul user_temp */
-		$sql1 = "SELECT `user`,`incercari`,`data` FROM `user_temp` WHERE `user`='$user'";
-		$result = mysql_query($sql1,self::$mysql->getResource());
+		// add the new try
 
+		$query = (
+			"SELECT `user`, `incercari`, `data`
+			FROM user_temp
+			WHERE user <:user"
+		);
 
-		if(!$result || (mysql_numrows($result)<1))
-		{
-			$sql1 = "INSERT INTO `user_temp` (user, ip, data) VALUES ('$user', '$ip', '$data')";
-			mysql_query($sql1, self::$mysql->getResource());
-			return 'continua';
+		$stmt = $db->prepare($query);
+		$ok = $stmt->execute(array(
+			'user' => $user
+		));
+
+		if(!$ok) {
+			throw new Exception("Ceva nu a mers cum trebuia");
 		}
-		else
-		{
-			/* Gaseste nr. incercari asociata user */
-			$tbarray = mysql_fetch_array($result);
-			$incercari = $tbarray['incercari'];
-			if ($incercari<5)
-			{
-				$incercari++;
-				// incrementeaza nr. incercari cu 1 și actualizeaza data
-				$sql1 = "UPDATE `user_temp` SET `incercari`='$incercari', `data`='$data' WHERE `user`='$user'";
-				$result = mysql_query($sql1, self::$mysql->getResource());
-				return 'continua';
+
+		$affectedRows = $stmt->rowCount();
+
+		if ($affectedRows == 0) {
+
+			$query = (
+				"INSERT INTO `user_temp` (user, ip, data)
+				VALUES (:user, :ip, :data)
+				"
+			);
+
+			$stmt = $db->prepare($query);
+			$ok = $stmt->execute(array(
+				'user' => $user,
+				'ip' => $ip,
+				'data' => $data
+			));
+
+			if(!$ok) {
+				throw new Exception("Ceva nu a mers cum trebuia");
 			}
-			else if ($incercari>=3)
-			{
+
+			return 'continua';
+		} else {
+			$tbarray   = mysql_fetch_array($result);
+			$incercari = $tbarray['incercari'];
+			if ($incercari < 5) {
+				$incercari++;
+
+				$query = (
+					"UPDATE user_temp
+					SET incercari=:incercari, `data`=:data
+					WHERE `user`=:user
+					"
+				);
+
+				$stmt = $db->prepare($query);
+				$ok = $stmt->execute(array(
+					'incercari' => $incercari,
+					'data' => $data,
+					'user' => $user
+				));
+
+				if(!$ok) {
+					throw new Exception("Ceva nu a mers cum trebuia");
+				}
+
+				return 'continua';
+			} else if ($incercari >= 3) {
 				$timp = 600 - ($data - $tbarray['data']);
-				return $timp;		// Indica nr. incercari depasit și returneaza timpul pt. calculare asteptare
+
+				return $timp;
 			}
 		}
 	}
-
-	/**
-	 *
-	 * Returneaza utilizatorul care este conectat
-	 * @return Utilizator 				Utilizatorul conectat la aplicatie
-	 *
-	 */
-	public function getUtilizator()
-	{
+	public function getUtilizator() {
 		return $this->utilizator;
 	}
 }
--- include/php/MYSQL.php
+++ include/php/MYSQL.php
@@ -1,75 +1,30 @@
 <?php
-
 require_once "MYSQL.php";
+class MYSQL {
+  private $resource_ = null;
+  private $utilizator = "yoy";
+  private $parola = "";
+  private $host = "localhost";
+  private $numeDB = "yoy_ro_date";
+  public function __construct($host, $utilizator, $parola, $numeDB) {
 
-/**
-*
-* Reprezinta obiectul care contine informatii despre conexiunea la baza de date
-* @author			Cristian Sima
-* @data			26.01.2014
-* @version			1.0
-*
-*/
-class MYSQL
-{
-	private $resource_ 		= null;
-	private $utilizator		= "yoy";
-	private $parola			= "";
-	private $host			= "localhost";
-	private $numeDB			= "yoy_ro_date";
+		$this->host       = $host;
 
+    $this->utilizator = $utilizator;
+    $this->parola     = $parola;
 
-	/**
-	*
-	* Realizeaza un obiect de conexiune la baza de date
-	*
-	* @param string $utilizator		Numele utilizatorului
-	* @param string $parola			Parola utilizatorului
-	* @param string $host				Adresa serverului
-	* @param string $bd				Numele bazei de date
-	*
-	* @throws Exception				Problema la conexiunea la baza de date
-	*
-	*/
-	public function __construct($host, $utilizator, $parola, $numeDB)
-	{
-		$this->utilizator 		= $utilizator;
-		$this->parola			= $parola;
-		$this->host				= $host;
-		$this->numeDB			= $numeDB;
-		@$this->resource_ 		= mysql_connect($host, $utilizator, $parola);
+    $this->numeDB     = $numeDB;
 
+		try {
+			$locator = 'mysql:host='.$host.';dbname='.$numeDB.';charset=utf8mb4';
 
-		if (!$this->resource_)
-		{
-			// MYSQL Exception: Conectare nereusita la MySQL
-			echo mysql_error();
-			throw new Exception ('Ceva nu a mers bine');
+			$this->resource_ = new PDO($locator, $utilizator, $parola);
+		} catch(PDOException $ex) {
+		    throw new Exception('Ceva nu a mers bine');
 		}
 
-		if (!mysql_select_db($this->numeDB, $this->getResource()))
-		{
-			echo mysql_error();
-			throw new Exception ('MYSQL Exception: Baza de date nu a putut fi selectata');
-		}
-
-		@$result = mysql_query("SET NAMES 'utf8'", $this->getResource());
-
-		if(!$result)
-		{
-			throw new Exception ('MYSQL Exception: Problema query');
-		}
-	}
-
-
-	/**
-	*
-	* Retuneaza resursa link a conexiunii
-	* @return 					Resursa link a conexiunii
-	*
-	*/
-	public function getResource()
-	{
-		return $this->resource_;
-	}
+  }
+  public function getResource() {
+    return $this->resource_;
+  }
 }
