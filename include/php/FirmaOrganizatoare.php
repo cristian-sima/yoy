bf85e508a6a594e48fa547871b4057a22883f4f4
cristian-sima <cs25g13@soton.ac.uk>
2016-11-12 15:26:57 +0200

    finish firma organizatoarea

--- include/php/FirmaOrganizatoare.php
+++ include/php/FirmaOrganizatoare.php
@@ -1,57 +1,36 @@
 <?php
-	
-	require_once "Firma.php";
-		
-	/**
-	 *	Encapsuleaza informatiile despre firma organizatoare
-	 *
-	 *  @author					Cristian Sima
-	 *  @date					27.01.2014
-	 *  @version				1.0
-	 *
-	 */
-	class FirmaOrganizatoare extends Firma
-	{		
-		private $patron			= null;
-		
-		
-		/**
-		 *
-		 * Realizeaza o noua firma organizatoare și stocheaza datele despre ea.
-		 * @param $MYSQL			Link spre resursa MYSQL
-		 * @param $id				ID-ul firmei
-		 * @throw Exception			Atunci cand firma nu exista
-		 *
-		 */
-		public function FirmaOrganizatoare($MYSQL, $id)
-		{
-			
-			$q = "SELECT nume AS denumire,patron, localitate AS locatie from `firma_organizatoare` WHERE id='".$id."' LIMIT 0,1";
-			$result = mysql_query($q, $MYSQL->getResource());
-			
-			if(mysql_num_rows($result) == 0)
-				throw new Exception("NoSuchFirmaOrganizatoare");
-				
-			while($firma = mysql_fetch_array($result))
-			{
-				$this->denumire 	= $firma['denumire'];
-				$this->locatie 		= $firma['locatie'];			
-				$this->patron 		= $firma['patron'];			
-			}
-			
-			$this->id = $id;
+require_once "Firma.php";
+class FirmaOrganizatoare extends Firma {
+  private $patron = null;
+  public function FirmaOrganizatoare($MYSQL, $id) {
+
+		$db = $MYSQL->getResource();
+
+		$query = (
+		  "SELECT nume AS denumire, patron, localitate AS locatie
+			FROM `firma_organizatoare`
+			WHERE id=:id
+			LIMIT 0,1"
+		);
+
+		$stmt = $db->prepare($query);
+		$ok = $stmt->execute(array(
+		  'id' => $id
+		));
+
+		if(!$ok) {
+		  throw new Exception("Această firmă organizatoare nu există");
 		}
-		
-		
-		
-		/**
-		 * 
-		 * Returneaza patronul firmei organizatoare
-		 * @return string			Patronul firmei organizatoare
-		 * 
-		 */
-		public function getPatron()
-		{
-			return $this->patron;
+
+		foreach($stmt as $row) {
+			$this->denumire = $row['denumire'];
+			$this->locatie  = $row['locatie'];
+			$this->patron   = $row['patron'];
 		}
-	}
+
+    $this->id = $id;
+  }
+  public function getPatron() {
+    return $this->patron;
+  }
+}
