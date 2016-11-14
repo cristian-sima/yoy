10a69c5c3d84176fbf60936b04791d8c48c3ba0d
cristian-sima <cs25g13@soton.ac.uk>
2016-11-14 21:16:02 +0200

    fix add company

--- add_company.php
+++ add_company.php
@@ -1,73 +1,221 @@
 <?php
-require 'app/Aplicatie.php';
-Design::showHeader();
 
-?>
+require_once 'app/Aplicatie.php';
 
-<table width="100%" id="heading">
-	<tr>
-		<td>
-			<h2 style="color: orange">
-				<img src="public/images/firme.png"  />Adaugă firmă
-			</h2>
-		</td>
-		<td style="text-align: right">
-			<input type="button" value="Înapoi la firme" onclick="document.location='companies.php'" />
-		</td>
-	</tr>
-</table>
-<div class="content">
-	<div id="dialog"></div>
-	<div id="form_wrapper" class="form_wrapper" style="width: 550px; display: block;">
-		<form id="f1" class="register active" style="" action="add_company_POST.php" method="POST">
-			<h3>
-				<cufon class="cufon cufon-canvas" alt="Register"
-				style="width: 106px; height: 25px;">
-				<canvas width="122"
-				height="28"
-				style="width: 122px; height: 28px; top: -2px; left: 0px;"></canvas>
-				<cufontext>Adaugă firmă</cufontext>
-			</cufon>
-		</h3>
-		<div class="column">
-			<div>
-				<label>Denumire:</label>
-				<input check="true" criteria='{type:"string",  minSize: "5", maxSize:"30"}' name="nume" type="text" id="nume">
-				<span class="error">Exista o
-					eroare</span>
-				</div>
-				<div>
-					<label>Localitate:</label>
-					<input id="localitate" name="localitate" check="true" criteria='{type:"string",minSize:"5",  maxSize:"30"}' type="text">
-					<span class="error">Exista o eroare</span>
-				</div>
-				<div>
-					<label>Procent câștiguri:</label>
-					<input check="true" criteria='{type:"numeric",  minSize: "1", maxSize:"3"}' id="procent" name="procent" type="text">
-					<span class="error">Exista
-						o eroare</span>
+try {
+	Design::showHeader();
+	$db = Aplicatie::getInstance()->Database;
+
+	?>
+	<div class="container">
+		<ol class="breadcrumb">
+			<li class="breadcrumb-item"><a href="companies.php">Firme spațiu</a></li>
+			<li class="breadcrumb-item active">Adaugă firmă</li>
+		</ol>
+		<div class="row">
+			<div class="col-lg-8 offset-lg-2 col-md-10 offset-md-1 col-xl-6 offset-xl-3">
+				<form id="company_form" action="add_company_POST.php" method="POST">
+					<h1>Adaugă firmă</h1>
+					<hr>
+					<div class="form-group text-xs-left row">
+						<label class="col-md-4 form-control-label" for="nume">
+							Denumire
+						</label>
+						<div class="col-md-8">
+							<input
+							class="form-control form-control-warning"
+							id="nume"
+							name="nume"
+							placeholder="ex. Impex S.R.L."
+							type="text"
+							value=""
+							/>
+						</div>
 					</div>
-				</div>
-				<div class="column">
-					<div>
-						<label>Comentarii:</label>
-						<textarea id="comentarii" check="true" name="comentarii" check="true" criteria='{type:"string",  maxSize:"30"}' type="text"></textarea>
-						<span class="error">Exista o eroare</span>
+					<div class="form-group text-xs-left row">
+						<label class="col-md-4 form-control-label" for="localitate">
+							Localitate
+						</label>
+						<div class="col-md-8">
+							<input
+							class="form-control form-control-warning"
+							id="localitate"
+							name="localitate"
+							placeholder="ex. Bolintin Vale"
+							type="text"
+							value=""
+							/>
+						</div>
 					</div>
-					<div>
-						<label>Date de contact:</label>
-						<textarea check="true" check="true" criteria='{type:"string",  maxSize:"30"}' id="date_contact" name="date_contact" type="text"></textarea>
-						<span class="error">Exista o eroare</span>
-						<i><small>Info: Vei putea adăuga administratorii ulterior.</small></i>
+					<div class="form-group text-xs-left row">
+						<label class="col-md-4 form-control-label" for="procent">
+							Procent câștiguri
+						</label>
+						<div class="col-md-8">
+							<div class="input-group">
+								<input
+								class="form-control form-control-warning"
+								id="procent"
+								name="procent"
+								placeholder="ex. 50"
+								type="number"
+								max="100"
+								min="0"
+								value="" />
+								<div class="input-group-addon">
+									%
+								</div>
+							</div>
+						</div>
 					</div>
-				</div>
-				<div class="column"></div>
-				<div class="bottom">
-					<input type="button" class="submit" onclick="checkForm('f1')" value="Firmă nouă" />
-					<div class="clear"></div>
+					<hr>
+					<div class="form-group text-xs-left row">
+						<label class="col-md-4 form-control-label" for="comentarii">
+							Comentarii
+						</label>
+						<div class="col-md-8">
+							<textarea
+							class="form-control form-control-warning"
+							id="comentarii"
+							name="comentarii"
+							placeholder="ex. Această firmă este în insolvență"
+							type="text"
+							value=""></textarea>
+						</div>
+					</div>
+					<div class="form-group text-xs-left row">
+						<label class="col-md-4 form-control-label" for="date_contact">
+							Date de contact
+						</label>
+						<div class="col-md-8">
+							<textarea
+							class="form-control form-control-warning"
+							check="true"
+							criteria='{type:"string",  maxSize:"30"}'
+							id="date_contact"
+							name="date_contact"
+							placeholder="ex. Strada Republicii Nr. 22"
+							type="text"
+							value=""></textarea>
+						</div>
+					</div>
+					<hr>
+					<div id="wrong-data-message" class="alert alert-warning" role="alert" style="display:none">
+						<strong>
+							Nu am putut trimite formularul
+						</strong>
+						Completează corect toate câmpurile
+					</div>
+					<div class="text-xs-center">
+						<button aria-label="Adaugă" class="btn btn-primary" type="submit">
+							Adaugă
+						</button>
 					</div>
 				</form>
 			</div>
-			<div class="clear"></div>
 		</div>
-		<?php Design::showFooter(); ?>
+	</div>
+
+	<?=	DESIGN::showFooter();	?>
+
+	<script type="text/javascript">
+	(function(){
+		$("#nume").focus();
+
+		$("#company_form").submit(function(event) {
+
+			const hasProblems = function () {
+				var isWrongName = function () {
+					var element = $("#nume"),
+					value = element.val(),
+					isWrong = (
+						value.length < 5 ||
+						value.length > 30
+					);
+
+					if (isWrong) {
+						element.focus();
+					}
+
+					return isWrong;
+				},
+				isWrongAddress = function () {
+					var element = $("#localitate"),
+					value = element.val(),
+					isWrong = (
+						value.length < 5 ||
+						value.length > 30
+					);
+
+					if (isWrong) {
+						element.focus();
+					}
+
+					return isWrong;
+				},
+				isWrongCurrentPercent = function () {
+					var element = $("#procent"),
+					value = element.val(),
+					isWrong = (
+						value === "" ||
+						isNaN(value)
+					);
+
+					if (isWrong) {
+						element.focus();
+					}
+
+					return isWrong;
+				},
+				isWrongComments = function () {
+					var element = $("#comentarii"),
+					value = element.val(),
+					isWrong = (
+						value.length > 30
+					);
+
+					if (isWrong) {
+						element.focus();
+					}
+
+					return isWrong;
+				},
+				isWrongContactDetails = function () {
+					var element = $("#date_contact"),
+					value = element.val(),
+					isWrong = (
+						value.length > 30
+					);
+
+					if (isWrong) {
+						element.focus();
+					}
+
+					return isWrong;
+				}
+
+				return (
+					isWrongName() ||
+					isWrongAddress() ||
+					isWrongCurrentPercent() ||
+					isWrongComments() ||
+					isWrongContactDetails()
+				)
+			};
+
+			if (hasProblems()) {
+				$("#wrong-data-message").fadeIn();
+				event.preventDefault();
+
+				return false;
+			}
+
+			return true;
+		})
+	})()
+	</script>
+
+	<?php
+} catch (Exception $e) {
+	DESIGN::complain($e->getMessage());
+}
